5fa47170eac2563fddfed0e7490b4a85b505b4b9
razibul.mithu <razibul.mithu@shikho.tech>
2022-08-28 00:16:17 +0600

    implementation for moc and test for controller and usecase

--- internal/app/adapter/controller/rate_test.go
+++ internal/app/adapter/controller/rate_test.go
@@ -1,1 +1,103 @@
 package controller
+
+import (
+	"encoding/json"
+	"github.com/golang/mock/gomock"
+	"github.com/historical-rate/internal/app/adapter"
+	"github.com/historical-rate/internal/app/application/usecase"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestLatestHistoricalRate(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	m := usecase.NewMockIHistoricalRate(ctrl)
+	m.EXPECT().GetLatest().Return(nil, nil)
+
+	server := Server{
+		DB:             nil,
+		RateRepository: m,
+	}
+	server.RateRepository = m
+	var response interface{}
+
+	req := httptest.NewRequest(http.MethodGet, "/rates/latest", nil)
+	res := httptest.NewRecorder()
+
+	server.GetLatestHistoricalRate(res, req, nil)
+	err := json.Unmarshal([]byte(res.Body.String()), &response)
+
+	if err != nil {
+		t.Error("Parse JSON Data Error")
+	}
+	if want, got := http.StatusOK, res.Result().StatusCode; want != got {
+		t.Fatalf("expected a %d, instead got: %d", want, got)
+	}
+
+	assert.Equal(t, res.Code, 200)
+}
+
+func TestHistoricalRateByDate(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	m := usecase.NewMockIHistoricalRate(ctrl)
+	date := "2022-01-01"
+	m.EXPECT().GetByDate(date).Return(nil, nil)
+
+	server := Server{
+		DB:             nil,
+		RateRepository: m,
+	}
+	server.RateRepository = m
+	var response interface{}
+
+	req := httptest.NewRequest(http.MethodGet, "/rates/"+date, nil)
+
+	res := httptest.NewRecorder()
+
+	server.GetHistoricalRateByDate(res, req, []adapter.Param{{
+		Key:   "date",
+		Value: date,
+	}})
+	err := json.Unmarshal([]byte(res.Body.String()), &response)
+
+	if err != nil {
+		t.Error("Parse JSON Data Error")
+	}
+	assert.Equal(t, res.Code, 200)
+}
+
+func TestHistoricalAnalyzeReport(t *testing.T) {
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	m := usecase.NewMockIHistoricalRate(ctrl)
+	m.EXPECT().GetAnalyze().Return(nil, nil)
+
+	server := Server{
+		DB:             nil,
+		RateRepository: m,
+	}
+	server.RateRepository = m
+	var response interface{}
+
+	req := httptest.NewRequest(http.MethodGet, "/rates/analyze", nil)
+	res := httptest.NewRecorder()
+
+	server.GetHistoricalAnalyzeReport(res, req, nil)
+	err := json.Unmarshal([]byte(res.Body.String()), &response)
+
+	if err != nil {
+		t.Error("Parse JSON Data Error")
+	}
+	if want, got := http.StatusOK, res.Result().StatusCode; want != got {
+		t.Fatalf("expected a %d, instead got: %d", want, got)
+	}
+
+	assert.Equal(t, res.Code, 200)
+}
--- internal/app/application/usecase/rate_test.go
+++ internal/app/application/usecase/rate_test.go
@@ -1,1 +1,82 @@
 package usecase
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/historical-rate/internal/app/domain"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestMockIHistoricalRate_GetLatestHistoricalRate(t *testing.T) {
+	ctrl := gomock.NewController(t)
+
+	defer ctrl.Finish()
+
+	m := NewMockIHistoricalRate(ctrl)
+	timeNow := time.Now()
+	var rates []domain.Rate
+	rates = append(rates, domain.Rate{
+		Id:       1,
+		Date:     &timeNow,
+		Currency: "BDT",
+		Rate:     100,
+	})
+	expectedResult := make(map[string]float32)
+	for _, value := range rates {
+		expectedResult[value.Currency] = value.Rate
+	}
+	m.EXPECT().GetLatest().Return(rates, nil)
+	res, err := RateUseCase{m}.GetLatestHistoricalRate()
+	assert.Equal(t, res, expectedResult)
+	assert.Equal(t, err, nil)
+}
+
+func TestRateUseCase_GetHistoricalRateByDate(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	m := NewMockIHistoricalRate(ctrl)
+	timeNow := time.Now()
+	timeString := timeNow.Format("2006-01-03")
+	var rates []domain.Rate
+	rates = append(rates, domain.Rate{
+		Id:       1,
+		Date:     &timeNow,
+		Currency: "BDT",
+		Rate:     100,
+	})
+
+	result := make(map[string]float32)
+	for _, value := range rates {
+		result[value.Currency] = value.Rate
+	}
+	m.EXPECT().GetByDate(timeString).Return(rates, nil)
+	res, err := RateUseCase{m}.GetHistoricalRateByDate(timeString)
+	assert.Equal(t, res, result)
+	assert.Equal(t, err, nil)
+}
+
+func TestMockIHistoricalRate_GetAnalyze(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	m := NewMockIHistoricalRate(ctrl)
+	var analyzes []domain.AnalyzeReport
+	analyzes = append(analyzes, domain.AnalyzeReport{
+		Currency: "BDT",
+		Min:      1,
+		Max:      3,
+		Avg:      2,
+	})
+	result := make(map[string]domain.AnalyzeReportResponse)
+	for _, value := range analyzes {
+		result[value.Currency] = domain.AnalyzeReportResponse{
+			Min: value.Min,
+			Max: value.Max,
+			Avg: value.Avg,
+		}
+	}
+	m.EXPECT().GetAnalyze().Return(analyzes, nil)
+	res, err := RateUseCase{m}.GetHistoricalAnalyzes()
+	assert.Equal(t, res, result)
+	assert.Equal(t, err, nil)
+}
