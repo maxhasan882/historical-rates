a338331ec5522955efc8c2fd13ec625edde4105d
razibul.mithu <razibul.mithu@shikho.tech>
2022-08-31 22:08:39 +0600

    error check with invalid data

--- internal/app/application/usecase/loader_test.go
+++ internal/app/application/usecase/loader_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestLoadData(t *testing.T) {
+func TestLoadDataWithValidData(t *testing.T) {
 	cubes := []domain.Cube{{
 		Time: "2016-01-02",
 		Cube: []struct {
@@ -39,4 +39,38 @@ func TestLoadData(t *testing.T) {
 	repo := mocks.DataLoaderMoc{}.Returns(historicalRates)
 	err := LoaderUseCase{LoadRepository: repo}.LoadData()
 	assert.Equal(t, err, nil)
+}
+
+func TestLoadDataWithUnValidData(t *testing.T) {
+	cubes := []domain.Cube{{
+		Time: "20-01-02",
+		Cube: []struct {
+			Currency string  `xml:"currency,attr"`
+			Rate     float64 `xml:"rate,attr"`
+		}{
+			{Currency: "BDT", Rate: 1.2},
+			{Currency: "BDT", Rate: 1.3},
+			{Currency: "BDT", Rate: 1.4},
+		},
+	},
+		{
+			Time: "2016-01-01",
+			Cube: []struct {
+				Currency string  `xml:"currency,attr"`
+				Rate     float64 `xml:"rate,attr"`
+			}{
+				{Currency: "BDT", Rate: 1.2},
+				{Currency: "BDT", Rate: 1.3},
+				{Currency: "BDT", Rate: 1.4},
+			},
+		},
+	}
+	historicalRates := domain.HistoricalRates{
+		Cube: struct {
+			Cube []domain.Cube `xml:"Cube"`
+		}{Cube: cubes},
+	}
+	repo := mocks.DataLoaderMoc{}.Returns(historicalRates)
+	err := LoaderUseCase{LoadRepository: repo}.LoadData()
+	assert.NotNil(t, err)
 }
