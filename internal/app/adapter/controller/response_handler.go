adae182a0b021d65a6201f85b14b16f241b1da18
razibul.mithu <razibul.mithu@shikho.tech>
2022-08-31 21:40:54 +0600

    handle response check

--- internal/app/adapter/controller/response_handler.go
+++ internal/app/adapter/controller/response_handler.go
@@ -8,9 +8,6 @@ import (
 func HandleBadRequest(w http.ResponseWriter, err error) {
 	w.WriteHeader(http.StatusBadRequest)
 	_, err = w.Write([]byte(err.Error()))
-	if err != nil {
-		return
-	}
 }
 
 func HandleSuccessResponse(w http.ResponseWriter, response map[string]interface{}) {
